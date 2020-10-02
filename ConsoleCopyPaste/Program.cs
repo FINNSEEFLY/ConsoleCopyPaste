6724794585bb7a8ed6fa3b95888dbfdd28057698
matmak122@gmail.com <FINNSEEFLY>
2020-10-02 18:35:23 +0300

    Added TaskQueue.cs

--- ConsoleCopyPaste/Program.cs
+++ ConsoleCopyPaste/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Threading;
+
 
 namespace ConsoleCopyPaste
 {
@@ -6,7 +9,56 @@ namespace ConsoleCopyPaste
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string srcPath = null, dstPath = null;
+            if (!ParseArgs(args, ref srcPath, ref dstPath))
+            {
+                Console.WriteLine("Не верные входные данные");
+                return;
+            }
+
+            if (!Directory.Exists(srcPath) || !Directory.Exists(dstPath))
+            {
+                Console.WriteLine("Некорректные директории");
+                return;
+            }
+
+            TaskQueue taskQueue = new TaskQueue(3);
+            CopyDirectory(srcPath, dstPath, taskQueue);
+            taskQueue.StartProcessing();
+            Console.ReadLine();
+            taskQueue.StopProcessing();
+        }
+
+        static bool ParseArgs(string[] args, ref string srcPath, ref string dstPath)
+        {
+            if (args == null || args.Length != 2 || args[0] == null || args[0] == "" ||
+                args[1] == null || args[1] == "") return false;
+            srcPath = args[0];
+            dstPath = args[1];
+            return true;
+        }
+
+        static void CopyDirectory(string srcPath, string dstPath, TaskQueue taskQueue)
+        {
+            var files = Directory.GetFiles(srcPath);
+            var directories = Directory.GetDirectories(srcPath);
+            dstPath = dstPath + @"\" + Path.GetFileName(srcPath);
+            Directory.CreateDirectory(dstPath);
+            foreach (var filePath in files)
+            {
+                taskQueue.EnqueueTask(() => CopyFile(filePath, dstPath + @"\" + Path.GetFileName(filePath)));
+            }
+
+            foreach (var directory in directories)
+            {
+                CopyDirectory(directory, dstPath, taskQueue);
+            }
+        }
+
+        static void CopyFile(string srcPath, string dstPath)
+        {
+            File.Copy(srcPath, dstPath, true);
+            Console.WriteLine("Файл [" + srcPath + "] был скопирован в [" + dstPath + "]");
         }
     }
 }
--- ConsoleCopyPaste/TaskQueue.cs
+++ ConsoleCopyPaste/TaskQueue.cs
@@ -1,7 +1,78 @@
-﻿namespace ConsoleCopyPaste
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleCopyPaste
 {
+    public delegate void TaskDelegate();
+
     public class TaskQueue
     {
-        
+        private Queue<TaskDelegate> _taskPull;
+        private List<Thread> _threadPull;
+        private bool isProcessing { get; set; }
+
+        public TaskQueue()
+        {
+            _taskPull = new Queue<TaskDelegate>();
+            _threadPull = new List<Thread>();
+            isProcessing = false;
+        }
+
+        public TaskQueue(int numOfThreads)
+        {
+            _taskPull = new Queue<TaskDelegate>();
+            _threadPull = new List<Thread>();
+            isProcessing = false;
+            for (var i = 0; i < numOfThreads; i++)
+            {
+                var thread = new Thread(ExecuteTask);
+                thread.Name = "Thread[" + i + "]";
+                _threadPull.Add(thread);
+            }
+        }
+
+        public void EnqueueTask(TaskDelegate task)
+        {
+            _taskPull.Enqueue(task);
+        }
+
+        public void StartProcessing()
+        {
+            isProcessing = true;
+            Thread processingThread = new Thread(Processing);
+            processingThread.Start();
+        }
+
+        public void StopProcessing()
+        {
+            isProcessing = false;
+        }
+
+        private void Processing()
+        {
+            while (isProcessing)
+            {
+                if (_taskPull.Count == 0) continue;
+                var task = _taskPull.Dequeue();
+                Thread thread = null;
+                while (thread == null && isProcessing)
+                {
+                    thread = _threadPull.FirstOrDefault(x => x.ThreadState==ThreadState.Unstarted);
+                    thread?.Start(task);
+                }
+            }
+        }
+
+        private static void ExecuteTask(object taskDelegate)
+        {
+            Console.WriteLine("Начал работу поток " + Thread.CurrentThread.Name);
+            TaskDelegate task = (TaskDelegate) taskDelegate;
+            task();
+            Console.WriteLine("Закончил работу поток " + Thread.CurrentThread.Name);
+        }
     }
 }
